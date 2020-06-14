6755416c729bf9596b0d2378e1f8aa7846ac6808
Pedro Amador <pedroamador.rodriguez@gmail.com>
2020-06-14 21:18:03 +0200

    Build: Enable remote deploy stage

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -12,6 +12,6 @@ git checkout .
 git fetch -pv
 git pull
 docker-compose pull
-docker-compose up -d
+docker-compose restart
 exit 0
 """ | ssh -t -t -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no master@${env}.huelladigital.ayudadigital.org bash -

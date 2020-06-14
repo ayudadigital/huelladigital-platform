8d763fd441bb96a01af5c4ffeafb9765bdc2ddc6
Pedro Amador <pedroamador.rodriguez@gmail.com>
2020-06-14 21:15:07 +0200

    Upgrade: Review router config and paths

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -5,13 +5,13 @@ env=$1
 echo """
 set -eu
 set -o pipefail
-cd platform
+cd huelladigital-platform
 git --no-pager diff
 git stash save "Deploy $(date '+%Y-%M-%d %H:%M:%S')"
 git checkout .
 git fetch -pv
 git pull
-docker-compose pull app
-docker-compose up -d app
+docker-compose pull
+docker-compose up -d
 exit 0
 """ | ssh -t -t -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no master@${env}.huelladigital.ayudadigital.org bash -

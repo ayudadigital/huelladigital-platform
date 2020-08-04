e266e7d38ba34a637c31606550c96f7e26a9ec7f
Pedro Amador <pedroamador.rodriguez@gmail.com>
2020-08-04 10:55:23 +0200

    Upgrade: Add web admin credentials to local and dev envs configuration

--- services/frontend/entrypoint.sh
+++ services/frontend/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 cd /workspace
 sed -i "s#http://backend:8080#$BASE_URI#g" $(grep -l -r "http://backend:8080" build/*)
+sed -i "s#http://localhost:3000#$BASE_URI#g" $(grep -l -r "http://localhost:3000" build/*)
 serve -s build
